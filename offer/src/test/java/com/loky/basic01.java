e0c5e35862a610acd17bce553ea353aaba4eacff
Rocky_Liang <2683737168@qq.com>
2021-11-05 14:54:46 +0800

    test1

--- offer/src/test/java/com/loky/basic01.java
+++ offer/src/test/java/com/loky/basic01.java
@@ -3,10 +3,10 @@ package com.loky;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 public class basic01 {
 
-	@Test
+
 	public void basic01(){
 		System.out.println(basic01.class);
 	}
